b401a3670a1f57c8ed953e9f4d866fbea6886c4a
Nikolay <pure0start@gmail.com>
2021-02-22 11:36:47 +0300

    Added some content to about route

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import * as React from 'react';
+
 interface AboutProps {}
 
 export function About(props: AboutProps) {
-  return null;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{'About us'}</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
