08e8a24b1781c9711b20625ba72049e0536ef357
Nikolay <pure0start@gmail.com>
2021-02-22 15:20:03 +0300

    Added TS interface for get rates request

--- src/api/rates.ts
+++ src/api/rates.ts
@@ -1,6 +1,21 @@
 import {fetchAPI, RequestMethodType} from './api';
 
-export function getRates() {
+interface GetRatesResponse {
+  [key: string]: {
+    baseVolume: string;
+    high24hr: string;
+    highestBid: string;
+    id: number;
+    isFrozen: string;
+    last: string;
+    low24hr: string;
+    lowestAsk: string;
+    percentChange: string;
+    quoteVolume: string;
+  };
+}
+
+export function getRates(): Promise<GetRatesResponse> {
   return fetchAPI('https://poloniex.com/public?command=returnTicker', {
     method: RequestMethodType.GET,
   });
